3515361da08c95ce612dc252b907d97fa8e3f9eb
Nodas <nodasxiros@gmail.com>
2022-07-19 14:46:14 +0300

    Removed typeorm as docs are outdated

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,27 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'email_client_db',
-      port: +process.env.POSTGRES_PORT,
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      migrations: ['dist/migrations/*.js'],
-      entities:['dist/**/*.entity{.ts,.js}'],
-      synchronize: false,
-      logging: 'all',
-    }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
-  constructor(private dataSource: DataSource) {}
 }
