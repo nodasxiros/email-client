409f23e017234484e0b63e6fb61156bc7810a694
Nodas <nodasxiros@gmail.com>
2022-07-22 02:02:42 +0300

    Updated home and signin pages

--- front/src/components/pages/Home.tsx
+++ front/src/components/pages/Home.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Home: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const accessToken = localStorage.getItem('access_token');
+  const [token, setToken] = useState<string | null>(accessToken);
+  useEffect(() => {
+    if (token)
+    setIsLoggedIn(true);
+  }, [token, isLoggedIn])
   return (
     <>
       <h1 style={{color: 'black'}}>Home Page</h1>
+      { !isLoggedIn && <h3>You are not logged in.</h3> }
+      { isLoggedIn && <h3>You are logged in!</h3> }
     </>
   )
 }
--- front/src/components/pages/SignIn.tsx
+++ front/src/components/pages/SignIn.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import SignInForm from '../forms/SignInForm'
 
 const SignIn: React.FC = () => {
   return (
     <>
       <h1 style={{color: 'black'}}>Sign In Page</h1>
+      <SignInForm />
     </>
   )
 }
