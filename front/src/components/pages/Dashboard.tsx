c2f6961a01749222d84450a8f2cea5e820b6dbda
Nodas <nodasxiros@gmail.com>
2022-07-22 02:02:13 +0300

    Updated Dashboard page

--- front/src/components/pages/Dashboard.tsx
+++ front/src/components/pages/Dashboard.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Table } from 'react-bootstrap';
+import client from '../../client';
+
+interface Email {
+  address: string,
+}
 
 const Dashboard: React.FC = () => {
+  const [emails, setEmails] = useState<Email[]>([])
+  const getEmails = async () => {
+    const { data } = await client.get('/emails');
+    console.log(data)
+  }
+  useEffect(() => {
+    getEmails()
+  });
   return (
     <>
       <h1 style={{color: 'black'}}>Dashboard Page</h1>
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>#</th>
+            {Array.from({ length: 12 }).map((_, index) => (
+              <th key={index}>Table heading</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          { }
+        </tbody>
+      </Table>
     </>
   )
 }
