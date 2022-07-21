ffc19f2f7bfaf62614dda16b954ca65286372eff
Nodas <nodasxiros@gmail.com>
2022-07-21 17:28:00 +0300

    Imported sequelize model inside messages module

--- api/src/messages/messages.module.ts
+++ api/src/messages/messages.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Message } from './entities/message.entity';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Message])],
   controllers: [MessagesController],
   providers: [MessagesService]
 })
